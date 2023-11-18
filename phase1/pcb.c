7ee62e4cb254024e7ef1c4168e9946cee2b1e35b
Diebbo <diego.barbieri03@libero.it>
2023-11-18 17:28:32 +0100

    miaomiao

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -5,12 +5,38 @@ LIST_HEAD(pcbFree_h);            /* List of free PCBs                     */
 static int next_pid = 1;
 
 void initPcbs() {
+    
 }
 
 void freePcb(pcb_t *p) {
+    // aggiungiamo alla lista dei processi eliminati
+    if(!list_empty(pcbFree_h))
+        INIT_LIST_HEAD(p);
+    else
+        __list_add_tail(p, pcbFree_h);
+
 }
 
 pcb_t *allocPcb() {
+    if(list_empty(pcbFree_h))
+        return NULL;
+
+    pcb_t *p = container_of(pcbFree_h->next);
+
+    list_del(pcbFree_h);
+
+    // ri-inizializzo l'elemento
+    p->p_list = NULL;
+    p->p_parent = NULL;
+    p->p_child = NULL;
+    p->p_sib = NULL;
+
+    p->p_s = 0;
+    p->p_time = 0;
+
+    p->msg_inbox = NULL;
+    p->p_supportStruct = NULL;
+    p->p_pid = 0;
 }
 
 void mkEmptyProcQ(struct list_head *head) {
