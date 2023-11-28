71e008b1e449dfc833ef1d91fb0599e53e0e5c2f
diebb0 <diego.barbieri03@libero.it>
2023-11-28 11:55:45 +0100

    removing comments

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -5,7 +5,7 @@ LIST_HEAD(pcbFree_h);            /* List of free PCBs                     */
 static int next_pid = 1;
 
 void initPcbs() {
-    // Initialize the list head for the free PCBs                           //##not sure
+    // Initialize the list head for the free PCBs                           
     INIT_LIST_HEAD(&pcbFree_h);
 
     for (int i = 0; i < MAXPROC; ++i) {
@@ -41,8 +41,7 @@ void initPcbs() {
 
 void freePcb(pcb_t *p) {
     if (list_empty(&pcbFree_h)) {                                           
-        // If the free list is empty, add the PCB directly as the head
-        // zio pera magari la inizializzerei sta coda se vuota @Axelredx
+        // If the free list is empty, initialize it
         INIT_LIST_HEAD(&pcbFree_h);
     }
     
@@ -52,15 +51,15 @@ void freePcb(pcb_t *p) {
 
 }
 
-pcb_t *allocPcb() {                                                         //##small tweaks
+pcb_t *allocPcb() {                                                         
     if (list_empty(&pcbFree_h))
         return NULL;
 
     struct list_head *head = pcbFree_h.next; // Get the first element
-    list_del(head); // Remove the PCB from the free list                    //##si necessita di una struct!!
+    list_del(head); // Remove the PCB from the free list                    
 
     // Convert the list_head to the PCB type
-    pcb_t *p = container_of(head, pcb_t, p_list);                           //## altri tweaks
+    pcb_t *p = container_of(head, pcb_t, p_list);                           
 
     // Reset PCB members if needed
     p->p_parent = NULL;
@@ -68,13 +67,13 @@ pcb_t *allocPcb() {                                                         //##
     INIT_LIST_HEAD(&p->p_child); 
     INIT_LIST_HEAD(&p->p_sib);   
     p->p_time = 0;
-    p->p_time = 0;                                                          //Ma zio pera Diebbo hai tolto p_s??
+    p->p_time = 0;                                                          
     INIT_LIST_HEAD(&p->msg_inbox); 
     p->p_supportStruct = NULL;
     p->p_pid = next_pid++;
 
     // Update p_list for the allocated PCB
-    INIT_LIST_HEAD(&p->p_list);                                            //## zio pera questo è importante!!
+    INIT_LIST_HEAD(&p->p_list);                                            
 
     return p;
 }
@@ -84,30 +83,29 @@ void mkEmptyProcQ(struct list_head *head) {
 }
 
 int emptyProcQ(struct list_head *head) {
-    return list_empty(head) ? 1 : 0;                                       //## ziopera le cose piccole mi fan paura >:o
+    return list_empty(head) ? 1 : 0;                                       
 }
 
 void insertProcQ(struct list_head* head, pcb_t* p) {
-    list_add_tail(&p->p_list, head);                                       //## li incoda!! (NO in testa)    
+    list_add_tail(&p->p_list, head);                                       
 }
 
 pcb_t* headProcQ(struct list_head* head) {
     if (emptyProcQ(head)) {
         return NULL; // Queue is empty, return NULL
-    } else {
-        // Traverse to the first element and return its pointer
-        return container_of(head->next, pcb_t, p_list);                    //##penso sia corretto no?
-    }    
+    } 
+    // Traverse to the first element and return its pointer
+    return container_of(head->next, pcb_t, p_list);                    
+        
 }
 
 pcb_t* removeProcQ(struct list_head* head) {
     if (list_empty(head)) {
         return NULL; // Queue is empty, return NULL
-    } else {
-        struct list_head *removed = head->next; 
-        list_del(removed); // Remove the element from the queue            //##same no?
-        return container_of(removed, pcb_t, p_list); 
-    }    
+    } 
+    struct list_head *removed = head->next; 
+    list_del(removed); // Remove the element from the queue       
+    return container_of(removed, pcb_t, p_list); 
 }
 
 pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
@@ -115,8 +113,8 @@ pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
         return NULL; // Invalid PCB pointer or empty queue, return NULL
     }
 
-    struct list_head *pos;                                                 //##ziopera qui mi dava prima un errore di ';'
-    list_for_each(pos, head) {                                             //## ma desso tuttapposto, wtf
+    struct list_head *pos;                                                 
+    list_for_each(pos, head) {                                           
         pcb_t *current_pcb = container_of(pos, pcb_t, p_list);
         if (current_pcb->p_pid == p->p_pid) {
             // Found the PCB to be removed, delete it from the queue
@@ -129,7 +127,7 @@ pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
 }
 
 int emptyChild(pcb_t *p) {
-    return list_empty(&p->p_child);                                         //##small is better (fortunately!!)
+    return list_empty(&p->p_child);                                         
 }
 
 void insertChild(pcb_t *prnt, pcb_t *p) {
@@ -143,8 +141,8 @@ void insertChild(pcb_t *prnt, pcb_t *p) {
         list_add_tail(&p->p_sib, &prnt->p_child);
     } else {
         // This is the first child, add it to the parent's child list
-        INIT_LIST_HEAD(&prnt->p_child);                                    //##DIebbo in questo project BISOGNA inizializz
-        list_add(&p->p_sib, &prnt->p_child);                               //##TUTTO (FORSE, poi ci confrontiamo)!
+        INIT_LIST_HEAD(&prnt->p_child);                                    
+        list_add(&p->p_sib, &prnt->p_child);                               
     }
     
     // Set the parent of the child
@@ -162,7 +160,7 @@ pcb_t* removeChild(pcb_t *p) {
 
     list_del(first_child);
     // Reinitialize the removed child's sibling pointer
-    INIT_LIST_HEAD(&removed_child->p_sib);                                 //## ok i pull up... ('AFTERPARTY', Don Toliver)
+    INIT_LIST_HEAD(&removed_child->p_sib);                                 
 
     removed_child->p_parent = NULL;
 
@@ -177,13 +175,13 @@ pcb_t* outChild(pcb_t *p) {
 
     struct list_head *pos;
     pcb_t *current_child;
-    list_for_each(pos, &p->p_parent->p_child) {                            //## manipolaz strana ma penso sia corretta
+    list_for_each(pos, &p->p_parent->p_child) {                      
         current_child = container_of(pos, pcb_t, p_sib);
         if (current_child->p_pid == p->p_pid) {
             list_del(pos);
             // Reinitialize the removed child's sibling pointer and child's list head
             INIT_LIST_HEAD(&p->p_sib); 
-            INIT_LIST_HEAD(&p->p_child);                                   //##FORSE (come prima)
+            INIT_LIST_HEAD(&p->p_child);              
             p->p_parent = NULL;
             return p;
         }
