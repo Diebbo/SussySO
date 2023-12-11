f154ba9674f745548f6d65f26d4be120b9633679
diebb0 <diego.barbieri03@libero.it>
2023-12-11 12:55:41 +0100

    commit inutile - non so se il make funzia

---
+++ phase2/src/main.c
@@ -0,0 +1,18 @@
+#ifndef MAIN
+#define MAIN
+
+#include "../headers/listx.h"
+#include "../headers/types.h"
+#include "../headers/const.h"
+
+#include "../phase1/headers/msg.h"
+#include "../phase1/headers/pcb.h"
+
+int main(){
+    // TODO: all
+    
+    return 0;
+}
+
+
+#endif MAIN
