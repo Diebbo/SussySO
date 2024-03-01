634f89d092bfcd5d119dac1a2245aef7e8a33c79
Axelredx <a109ros@gmail.com>
2024-03-01 21:06:52 +0100

    diebbo avev fatto bordello altrochè

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -29,105 +29,70 @@ void exceptionHandler() {
 }
 
 void SYSCALLExceptionHandler() {
-  // finding if in user or kernel mode
-  state_t *exception_state = (state_t *)BIOSDATAPAGE;
-  int a0 = exception_state->reg_a0, a1 = exception_state->reg_a1,
-      a2 = exception_state->reg_a2, a3 = exception_state->reg_a3,
-      user_state = exception_state->status;
+		// finding if in user or kernel mode
+	state_t *exception_state = (state_t *)BIOSDATAPAGE;
+	int a0 = exception_state->reg_a0, a1 = exception_state->reg_a1,
+		a2 = exception_state->reg_a2, a3 = exception_state->reg_a3,
+		user_state = exception_state->status;
 
-  if (a0 >= -2 && a0 <= -1) {
-    if (user_state == 1) { // kernel state syscall
-      switch (a0) {
-      case SENDMESSAGE:
-        /*If the target process is in the pcbFree_h list, set the return
-           register (v0 in µMPS3) to DEST_NOT_EXIST. If the target process is in
-           the ready queue, this message is just pushed in its inbox, otherwise
-           if the process is awaiting for a message, then it has to be awakened
-           and put into the Ready Queue.*/
+	if (a0 >= -2 && a0 <= -1) {
+		if (user_state == 1) { // kernel state syscall
+			switch (a0) {
+				case SENDMESSAGE:
+					/*If the target process is in the pcbFree_h list, set the return
+					register (v0 in µMPS3) to DEST_NOT_EXIST. If the target process is in
+					the ready queue, this message is just pushed in its inbox, otherwise
+					if the process is awaiting for a message, then it has to be awakened
+					and put into the Ready Queue.*/
 
-        // TODO
+					// TODO
 
-        /*on success returns/places 0 in the caller’s v0, otherwise
-                MSGNOGOOD is used to provide a meaningful error condition
-                on return*/
-        exception_state->reg_a3 = SYSCALL(SENDMESSAGE, a1, a2, 0);
+					/*on success returns/places 0 in the caller’s v0, otherwise
+							MSGNOGOOD is used to provide a meaningful error condition
+							on return*/
+					exception_state->reg_a3 = SYSCALL(SENDMESSAGE, a1, a2, 0);
 
-        break;
-      case RECEIVEMESSAGE:
-        /*the sender process PCB address or ANYMESSAGE in a1, a pointer to an
-        area where the nucleus will store the payload of the message in a2 (NULL
-        if the payload should be ignored) and then executing the SYSCALL
-        instruction. If a1 contains a ANYMESSAGE pointer, then the requesting
-        process is looking for the first message in its inbox, without any
-        restriction about the sender. In this case it will be frozen only if the
-        queue is empty, and the first message sent to it will wake up it and put
-        it in the Ready Queue.*/
+					break;
+				case RECEIVEMESSAGE:
+					/*the sender process PCB address or ANYMESSAGE in a1, a pointer to an
+					area where the nucleus will store the payload of the message in a2 (NULL
+					if the payload should be ignored) and then executing the SYSCALL
+					instruction. If a1 contains a ANYMESSAGE pointer, then the requesting
+					process is looking for the first message in its inbox, without any
+					restriction about the sender. In this case it will be frozen only if the
+					queue is empty, and the first message sent to it will wake up it and put
+					it in the Ready Queue.*/
 
-<<<<<<< HEAD
-				exception_state->reg_a3=SYSCALL(RECEIVEMESSAGE,a1,a2,0);
-		
-			default:
-                // Unhandled SYSCALL, simulate Program Trap exception in user-mode
-                // Set Cause.ExcCode to RI (Reserved Instruction)
-                exception_state->cause = (exception_state->cause & ~GETEXECCODE) | (RI << CAUSESHIFT); //RI non esiste???
-				/*exception_state->cause & ~GETEXECCODE: This part clears the bits related to the exception code in the cause field. 
-					It uses the bitwise NOT (~) operator to create a bitmask where all bits are set to 1 except for the bits in the GETEXECCODE position, 
-					and then performs a bitwise AND with the existing cause field to clear those bits.
-					| (RI << CAUSESHIFT): This part sets the exception code to RI (Reserved Instruction) by shifting the value of RI left by CAUSESHIFT bits 
-					and performing a bitwise OR with the existing cause field.*/
-                    
-                // Call Program Trap exception handler
-                TrapExceptionHandler();
-                break;
+							exception_state->reg_a3=SYSCALL(RECEIVEMESSAGE,a1,a2,0);
+					
+				default:
+					// Unhandled SYSCALL, simulate Program Trap exception in user-mode
+						// Set Cause.ExcCode to RI (Reserved Instruction)
+						exception_state->cause = (exception_state->cause & ~GETEXECCODE) 
+													| (RI << CAUSESHIFT); //RI non esiste???
+							/*exception_state->cause & ~GETEXECCODE: This part clears the bits related to the exception code 
+							in the cause field. It uses the bitwise NOT (~) operator to create a bitmask where all bits are 
+							set to 1 except for the bits in the GETEXECCODE position, and then performs a bitwise AND with the 
+							existing cause field to clear those bits.
+							| (RI << CAUSESHIFT): This part sets the exception code to RI (Reserved Instruction) by shifting the 
+							value of RI left by CAUSESHIFT bits and performing a bitwise OR with the existing cause field.*/
+							
+						// Call Program Trap exception handler
+						TrapExceptionHandler();
+						break;
 			}
-            // Returning from SYSCALL
-            // Increment PC by 4 to avoid an infinite loop of SYSCALLs +//load back updated interrupted state 
-            exception_state->pc_epc += WORDLEN;
-			LDST(exception_state);
+				// Returning from SYSCALL
+				// Increment PC by 4 to avoid an infinite loop of SYSCALLs +//load back updated interrupted state 
+				exception_state->pc_epc += WORDLEN;
+				LDST(exception_state);
+			}else{
+				// Process is in user mode, simulate Program Trap exception
+				// Set Cause.ExcCode to RI (Reserved Instruction)
+				exception_state->cause = (exception_state->cause & ~GETEXECCODE) | (RI << CAUSESHIFT); //RI non esiste???
+				
+				TrapExceptionHandler();
+			}
 		}else{
-        	// Process is in user mode, simulate Program Trap exception
-        	// Set Cause.ExcCode to RI (Reserved Instruction)
-        	exception_state->cause = (exception_state->cause & ~GETEXECCODE) | (RI << CAUSESHIFT); //RI non esiste???
-            
-            TrapExceptionHandler();
+			TrapExceptionHandler();
 		}
-	}else{
-=======
-        // TODO
-
-        exception_state->reg_a3 = SYSCALL(RECEIVEMESSAGE, a1, a2, 0);
-
-      default:
-        // Unhandled SYSCALL, simulate Program Trap exception in user-mode
-        // Set Cause.ExcCode to RI (Reserved Instruction)
-        exception_state->cause = (exception_state->cause & ~GETEXECCODE) |
-                                 (RI << CAUSESHIFT); // RI non esiste???
-        /*exception_state->cause & ~GETEXECCODE: This part clears the bits
-           related to the exception code in the cause field. It uses the bitwise
-           NOT (~) operator to create a bitmask where all bits are set to 1
-           except for the bits in the GETEXECCODE position, and then performs a
-           bitwise AND with the existing cause field to clear those bits. | (RI
-           << CAUSESHIFT): This part sets the exception code to RI (Reserved
-           Instruction) by shifting the value of RI left by CAUSESHIFT bits and
-           performing a bitwise OR with the existing cause field.*/
-
-        // Call Program Trap exception handler
->>>>>>> cb04e003f53bb16a1055f813852bec73e03413ce
-        TrapExceptionHandler();
-        break;
-      }
-      // Returning from SYSCALL
-      // Increment PC by 4 to avoid an infinite loop of SYSCALLs
-      exception_state->pc_epc += WORDLEN;
-    } else {
-      // Process is in user mode, simulate Program Trap exception
-      // Set Cause.ExcCode to RI (Reserved Instruction)
-      exception_state->cause = (exception_state->cause & ~GETEXECCODE) |
-                               (RI << CAUSESHIFT); // RI non esiste???
-
-      TrapExceptionHandler();
-    }
-  } else {
-    TrapExceptionHandler();
-  }
 }
