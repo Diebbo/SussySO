078989937cf1562cc307f470a6e4ddc922e8434c
Diebbo <diego.barbieri03@libero.it>
2024-03-30 17:18:10 +0100

    ssi refactor typos

--- phase2/ssi.c
+++ phase2/ssi.c
@@ -5,31 +5,17 @@ void SSI_function_entry_point() {
   msg_PTR process_request_msg;
   while (TRUE) {
     // receive request (asked from ssi proc; payload is temporaly not important)
-    SYSCALL(RECEIVEMESSAGE, ANYMESSAGE, 0, 0);
-
-    // current process == ssi_pcb
-    // get request id
-    unsigned int process_request_id = ssi_pcb->p_s.reg_a0;
-
-    // find process
-    process_request_ptr = findProcessPtr(&ready_queue_list, process_request_id);
-
-    if (process_request_ptr == NULL) {
-      process_request_ptr = findProcessPtr(&msg_queue_list, process_request_id);
-    }
-
-    if (process_request_ptr == NULL) {
-      // process not found -> look for the next message
-      continue;
-    }
+    pcb_PTR process_request = (pcb_PTR) SYSCALL(RECEIVEMESSAGE, ANYMESSAGE, 0, 0);
 
     // find msg payload
     process_request_msg =
-        popMessageByPid(&process_request_ptr->msg_inbox, process_request_id);
+        popMessageByPid(&process_request_ptr->msg_inbox, process_request->p_pid);
 
+
+    ssi_payload_PTR process_request_payload = process_request_msg->m_payload;
     // satysfy request and send back resoults(with a SYSYCALL in SSIRequest)
-    SSI_Request(process_request_ptr, process_request_ptr->p_s.reg_a2,
-                (void *)process_request_msg->m_payload);
+    SSI_Request(process_request_ptr, process_request_payload->service_code,
+                process_request_payload->arg);
   }
 }
 
