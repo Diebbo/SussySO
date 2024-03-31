0c46514533f2f28fab93d65a3443a3dcfc82170b
VittorioInc <vittorio.incerti@studio.unibo.it>
2024-03-31 18:50:21 +0200

    prova

--- phase2/ssi.c
+++ phase2/ssi.c
@@ -146,7 +146,7 @@ void *DoIO(pcb_t *sender, ssi_do_io_PTR arg) {
   *arg->commandAddr =
       arg->commandValue; // !IMPORTANT: this rise an interrupt exception from
                            // a device
-
+  setCAUSE(PRINTINTERRUPT);
   return NULL;
 }
 
